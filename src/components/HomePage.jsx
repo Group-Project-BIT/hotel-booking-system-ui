9abaefc0fc260bd8de1a8216e6bfae1d72e48bce
Ashen <ashendeelakahhk@gmail.com>
2024-03-31 10:14:18 +0530

    installed tailwind css,matirail ,icons library

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,13 @@
+"use client"
+import { Typography } from "@material-tailwind/react";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div >
+      <Typography>Homepage</Typography>
+    </div>
+  );
+};
+
+export default HomePage;
