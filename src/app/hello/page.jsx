5a4d67ce41e8e14d3d94d86f2b7eaf34e4c2383d
Ashen <ashendeelakahhk@gmail.com>
2024-04-01 13:01:21 +0530

    tailwind bug

---
+++ src/app/hello/page.jsx
@@ -0,0 +1,11 @@
+"use client"
+import { Typography } from '@material-tailwind/react'
+import React from 'react'
+
+const Hello = () => {
+  return (
+    <div className='p-13'><Typography>hii</Typography></div>
+  )
+}
+
+export default Hello
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+import withMT from "@material-tailwind/react/utils/withMT";
+ 
+export default withMT({
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+});
