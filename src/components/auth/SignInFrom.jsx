1457dff356652ca31821d053c0119364051ec70d
Ashen <ashendeelakahhk@gmail.com>
2024-05-28 10:59:21 +0530

    continue sign signup

--- src/components/auth/SignInFrom.jsx
+++ src/components/auth/SignInFrom.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
-
+"use client";
+import React from "react";
+import {
+  Card,
+  Input,
+  Checkbox,
+  Button,
+  Typography,
+} from "@material-tailwind/react";
 const SignInFrom = () => {
   return (
-    <div>SignInFrom</div>
-  )
-}
+      <Card color="transparent" shadow={false}>
+        <Typography variant="h4" color="blue-gray">
+          Sign In
+        </Typography>
+        <Typography color="gray" className="mt-1 font-normal">
+          Nice to meet you again! Enter your details to login.
+        </Typography>
+        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+          <div className="mb-1 flex flex-col gap-6">
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Email
+            </Typography>
+            <Input
+              size="lg"
+              placeholder="name@mail.com"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+            />
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Password
+            </Typography>
+            <Input
+              type="password"
+              size="lg"
+              placeholder="********"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+            />
+          </div>
+          <Button className="mt-6" fullWidth>
+            sign in
+          </Button>
+          <Typography color="gray" className="mt-4 text-center font-normal">
+            Already haven't an account?{" "}
+            <a href="/auth/sign-up" className="font-medium text-gray-900">
+              Sign Up
+            </a>
+          </Typography>
+        </form>
+      </Card>
+    );
+  };
+  
 
 export default SignInFrom
