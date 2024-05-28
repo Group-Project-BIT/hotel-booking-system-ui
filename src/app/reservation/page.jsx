93b78dabb76bdee0236273c087c4248f68f29ab8
Manoj Dilshan <krmdp@outlook.com>
2024-05-28 10:47:30 +0530

    implemented steper

---
+++ src/app/reservation/page.jsx
@@ -0,0 +1,13 @@
+"use client"
+import { StepperWithContent } from '@/components/configaration/StepperWithContent'
+import React from 'react'
+
+const ConfigarationPage = () => {
+  return (
+    <div className='flex flex col justify-center items-center h-screen'>
+        <StepperWithContent/>
+    </div>
+  )
+}
+
+export default ConfigarationPage
